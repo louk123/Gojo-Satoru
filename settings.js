20c0ef6e04e801ed7b9f95fff136230370879b7c
louk123 <106319242+louk123@users.noreply.github.com>
2022-07-19 11:34:37 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
+global.owner = ['212661809542']
 global.premium = ['Your Number']
 global.ownernomer = 'Your Number'
 global.ownername = '🐦Star Boy Nexus'
